c9575483691153b871fb5afaae0bfc20e518292f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-05-05 05:44:22 -0300

    Python

--- havard-CS50-Python&Javascript/third-course-Python/decorators/decorators.py
+++ havard-CS50-Python&Javascript/third-course-Python/decorators/decorators.py
@@ -1,2 +1,12 @@
-def announce():
+def announce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with function.")
+    return wrapper
 
+@announce
+def hello():
+    print("Hello, world!")
+
+hello()
