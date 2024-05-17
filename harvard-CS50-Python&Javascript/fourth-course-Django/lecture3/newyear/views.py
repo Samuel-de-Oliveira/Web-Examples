741586de2573e20ffc9c132dd35ecd15e2f68df1
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-05-17 13:44:33 -0300

    Include new app

--- harvard-CS50-Python&Javascript/fourth-course-Django/lecture3/newyear/views.py
+++ harvard-CS50-Python&Javascript/fourth-course-Django/lecture3/newyear/views.py
@@ -3,5 +3,6 @@ from datetime import datetime
 
 def index(request):
     return render(request, "newyear/index.html", {
-            "newyear": f"{datetime.now().day}/{datetime.now().month}/{datetime.now().year}"
+            "nowdate": f"{datetime.now().day}/{datetime.now().month}/{datetime.now().year}",
+            "newyear": datetime.now().month == 1
 })
