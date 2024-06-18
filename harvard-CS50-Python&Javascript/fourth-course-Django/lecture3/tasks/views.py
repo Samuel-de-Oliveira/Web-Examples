498d5d0084c43b480d87606bf3ca3eae416a4732
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-06-18 15:52:53 -0300

    Update

--- harvard-CS50-Python&Javascript/fourth-course-Django/lecture3/tasks/views.py
+++ harvard-CS50-Python&Javascript/fourth-course-Django/lecture3/tasks/views.py
@@ -1,18 +1,40 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-tasks = ["foo", "bar", "baz", "boo", "mun"]
+tasks = []
+
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label="New task")
+    priority = forms.IntegerField(
+        label="Priority",
+        min_value=1,
+        max_value=10
+    )
+
 
 def index(request):
     return render(request, "tasks/index.html", {
-                'tasks': tasks
-            })
+        'tasks': tasks
+    })
 
 
 def add(request):
-    return render(request, "tasks/add.html", {
-                'form': NewTaskForm()
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+
+        else:
+            return render(request, "task/add.html", {
+                'form': form
             })
+
+    return render(request, "tasks/add.html", {
+        'form': NewTaskForm()
+    })
