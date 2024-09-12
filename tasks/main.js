90bbf66871dce2f974bffb85f113b5e13173349f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-09-12 12:43:39 -0300

    Add Javascript

--- tasks/main.js
+++ tasks/main.js
@@ -1,3 +1,20 @@
 var taskList = document.querySelector("#task-list")
 
-taskList.innerHTML = "<li>None</li>"
+var listOfTasks = [] 
+
+document.querySelector('#new-task').onclick = () => {
+   var theTask = window.prompt('Your task:')
+   
+   if (theTask) {
+      var li = document.createElement('li')
+      li.innerHTML = theTask
+      listOfTasks.push(li)
+
+      taskList.innerHTML = ""
+      for (let i = 0; i < listOfTasks.length; i++) {
+        taskList.appendChild(listOfTasks[i])
+      }
+   } else {
+      window.alert('Oh no, please digit a valid task!')
+   }
+}
