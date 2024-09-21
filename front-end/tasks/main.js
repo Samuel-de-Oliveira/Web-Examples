33d285d6fd29af5c8baa72ec8cc35db44bef7bb0
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-09-21 10:13:00 -0300

    Update Tasks

--- front-end/tasks/main.js
+++ front-end/tasks/main.js
@@ -3,8 +3,9 @@ const taskList = document.querySelector("#task-list");
 var listOfTasks = [];
 var taskID = 0;
 
+// Create <b>Empty...</b>
 if (listOfTasks.length <= 0) {
-   taskList.innerHTML = "<li><b>Empty...</b></li>";
+   taskList.innerHTML = "<li id=\"empty\"><b>Empty...</b></li>";
 }
 
 // Create a new task
@@ -20,13 +21,29 @@ document.querySelector('#new-task').onclick = () => {
       li.innerHTML = theTask;
       li.classList.add('appended-Task');
 
+      // Clean the <b>Empty...</b>
+      if (taskList.children.length >= 1) {
+         try {
+            document.querySelector('#empty').remove()
+         } catch {
+            // Nothing yet
+         }
+      }
+
       // Append the new task
       taskList.appendChild(li);
 
-      let listChildren = taskList.children;
-      if (listChildren.length >= 1) {
-         // Nothing yet
-      }
+      
+      // Add remove function
+      let allTasks = document.querySelectorAll(".appended-Task").forEach(
+         element => {
+            element.onclick = () => {
+               // Remove element
+               element.remove()
+            }
+         }
+      );
+
    } else {
       // In case of prompt error
       window.alert('Oh no, please digit a valid task!');
