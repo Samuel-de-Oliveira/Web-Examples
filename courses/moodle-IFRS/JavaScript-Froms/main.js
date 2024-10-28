2a942c40f9a6358245bde1a382a171489219b2d1
Samuel-de-Oliveira <samwolfg13@gmail.com>
2024-10-28 10:05:34 -0300

    New app

--- courses/moodle-IFRS/JavaScript-Froms/main.js
+++ courses/moodle-IFRS/JavaScript-Froms/main.js
@@ -4,3 +4,12 @@ document.body.onresize = () => {
   console.log(`${Width}X${Height}`);
   document.querySelector("#resize").innerHTML = `Screen: ${Width}X${Height}`;
 };
+
+document.querySelector('#formulario').onsubmit = () => {
+  const resultado = document.querySelector('#resultado');
+  const massa     = document.querySelector('#massa');
+  const altura    = document.querySelector('#altura');
+
+  let IMC = parseFloat(massa.value) / (parseFloat(altura.value) * 2);
+  resultado.innerHTML = `Seu IMC é de ${IMC.toFixed(2)}`;
+};
