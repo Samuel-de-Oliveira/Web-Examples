4f89b9ac9320ac818fdf2570653c9e0f376376b0
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-01-02 08:42:40 -0300

    Fix

--- front-end/Vue/Temperature-Calculator/scripts/app.js
+++ front-end/Vue/Temperature-Calculator/scripts/app.js
@@ -4,36 +4,41 @@ const Components = {
   data() {
     return {
       hello: Vue.ref('Hello!'),
-      counter: Vue.ref(0),
+      counter: Vue.ref(15),
+      Temperature: Vue.ref(0),
     }
   },
 
   // App Methods
   methods: {
+    // Main Method
     showTemperature() {
-      var text = `Hello world ${this.counter}`;
-      var convertion = this['Cel2Fah']
+      var input = parseInt(document.querySelector("#getvalue").value);
+      var convertion = this['Cel2Fah'];
       this.counter++;
-      document.querySelector("#text").innerHTML = text + ` ` + convertion(this.counter);
+      this.Temperature = `Result ${convertion(input).toFixed(2)}`;
+      this.hello = input;
     },
 
+    // Temperature Methods
     Cel2Fah(input) {
-      return (5 * (input + 32)) / 9
+      return (9/5 * input) + 32;
     },
     Fah2Cel(input) {
-      return (9 * (input - 32)) / 5
+      return (5/9 * (input - 32));
     },
     Cel2Kel(input) {
-      return input + 273
+      return input + 273;
     },
     Kel2Cel(input) {
-      return input - 273
+      return input - 273;
     },
     Fah2Kel(input) {
-      return ((input + 459) * 5) / 9
+      // TODO: Consertar Conversor de Fah e Kel
+      return ((input + 459) * 5) / 9;
     },
     Kel2Fah(input) {
-      return ((input - 495) * 9) / 5
+      return ((input - 495) * 9) / 5;
     },
   },
 };
