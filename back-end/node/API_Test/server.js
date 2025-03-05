f45361be47d1852269de342ebefdf89d8d388540
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-03-04 21:24:46 -0300

    Test my API

--- back-end/node/API_Test/server.js
+++ back-end/node/API_Test/server.js
@@ -1,15 +1,23 @@
 import express from 'express'
 
-const app = express(); // Get the app object
+const app   = express(); // Get the app object
+const Port  = 3000;
+
+let Values = {
+  home: '/',
+  counter: 0,
+};
 
 // Home
 app.get('/', (req, res) => {
+  console.log(Values.counter + " " + req);
+  Values.counter++; 
   res.send('<h1>Hello, world!</h1>');
 });
 
 // Users
 app.get('/users', (req, res) => {
-  res.send('<h1>There\'s no users for now!</h1>')
+  res.json(Values);
 });
 
-app.listen(3000); // Turn on the server
+app.listen(Port); // Turn on the server
