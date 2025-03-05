98f1fe4f9f5ffe2c84fde792bb789663fb277918
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-03-05 11:43:05 -0300

    .

--- back-end/node/API_Test/server.js
+++ back-end/node/API_Test/server.js
@@ -8,20 +8,22 @@ var Values = {
 };
 
 // Home
-app.use(express.static('./assets/public')) // export public files to '/'
-app.get('/', (req, res) => {
-  console.log(Values.Visits_counter + " " + req);
+app.use('/', express.static('./assets/public')); // export public files to '/'
+app.get('/count', (req, res, next) => {
+  // TODO: Make this part of code work
+  console.log(Values.Visits_counter + " " + req.url);
   Values.Visits_counter++;
-});
-app.post('/', (req, res) => {
-  console.log('Ceci n\'est pas un POST...');
-  res.send('Ceci n\'est pas un POST...');
+  res.send('Counted 1 visit to the system!');
 });
 
 // Users
 app.get('/users', (req, res) => {
   // Send a JSON file
   res.json(Values);
+});
+app.get('/users/:name', (req, res) => {
+  console.log(`Username: ${req.params.name}`);
+  res.send(`Username: ${req.params.name}`);
 });
 
 // Turn on the server
