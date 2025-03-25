a9b0b3a85dbe1d26df497e3284148b007470df79
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-03-25 09:54:53 -0300

    Test Django features

--- back-end/python/mysite/home/urls.py
+++ back-end/python/mysite/home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('visits/', views.visits, name="visits"),
+    path('random/', views.random, name="random"),
 ]
--- back-end/python/mysite/home/views.py
+++ back-end/python/mysite/home/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render, HttpResponse
+from random import randint
+
+colors: list = [
+    'blue',
+    'gray',
+    'red',
+    'yellow',
+    'green',
+]
 
 Status: dict = {
         'visits': 0,
 }
 
-def index(req) -> HttpResponse:
+# Index
+def index(req) -> render:
     Status['visits'] += 1
     
-    return render(req, 'index.html', {
-        'visits': Status['visits']
-    })
+    return render(
+        req, 'index.html', {
+            'visits': Status['visits']
+        }
+    )
+
+# Visits
+def visits(req) -> HttpResponse:
+    return HttpResponse(f'Visits: {Status['visits']}')
+
+# Random
+def random(req) -> render:
+    return render(
+        req, 'random/index.html', {
+            'color': colors[randint(0, len(colors) - 1)].capitalize()
+        }
+    )
