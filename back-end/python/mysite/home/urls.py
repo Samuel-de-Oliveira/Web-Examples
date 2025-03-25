1b58097720346debc468cae0b2f403b67c5a12a8
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-03-25 10:46:49 -0300

    Test Django features

--- back-end/python/mysite/home/urls.py
+++ back-end/python/mysite/home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('visits/', views.visits, name="visits"),
     path('random/', views.random, name="random"),
-]
+] + static(settings.STATIC_URL)
