8c92a150057774a9ef112d2464c4621c4399e25e
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-03-30 13:01:58 -0300

    Teste page style

--- back-end/node/School-system/server.js
+++ back-end/node/School-system/server.js
@@ -27,11 +27,26 @@ app.use('/static', express.static(__dirname + '/static'));
 /// Home ///
 app.get('/home', (req, res) => {
   res.render('homePage', {
-    schoolName: 'Colégio alfa',
+    title: 'Colégio alfa',
     stylePath:  '/static/website-style.css',
+    pageInfo: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>",
   });
 });
 ////////////
+
+/// Users ///
+app.get('/u', (req, res) => {
+  res.send('No user especified!');
+});
+
+app.get('/u/:username', (req, res) => {
+  let username = req.params.username;
+  if (username) {
+    res.send(`Hello from ${username}`);
+  }
+});
+////////////
+
 
 /// Run Server ///
 app.listen(PORT, (err) => {
