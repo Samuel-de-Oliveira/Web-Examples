b8590c298ff75a97bf4a6102bf4b0670086c868f
Samuel-de-Oliveira <samwolfg13@gmail.com>
2025-04-08 10:03:59 -0300

    Testing flask framework

---
+++ back-end/python/Flask/hello/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, url_for, render_template
+from markupsafe import escape
+from random import randint
+
+backgrounds: list = [
+    'white',
+    'blue',
+    'red',
+    'green',
+    'yellow',
+    'gray',
+]
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> str:
+    return render_template('index.hbs')
+
+
+@app.route('/<username>')
+def show_name(username: str) -> str:
+    background_number: int = randint(0, len(backgrounds) - 1)
+    print(backgrounds[background_number])
+    return render_template(
+        'hello.hbs',
+        username=escape(username),
+        background_name=backgrounds[background_number],
+    )
